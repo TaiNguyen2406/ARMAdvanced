2d2cbcb8aaccf1ed2d2e56ec8298e3a1119cf8c2
nguyễn duy tài <underwaterhp93@gmail.com>
2019-12-16 00:27:53 +0700

    add VM

--- deployDev.ps1
+++ deployDev.ps1
@@ -1,9 +1,14 @@
-﻿$templateFile=".\azuredeploy.json"
-New-AzResourceGroup `
-  -Name myResourceGroupDev `
-  -Location "Southeast Asia"
+﻿$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$adminUsername = Read-Host -Prompt "Enter the virtual machine admin username"
+$adminPassword = Read-Host -Prompt "Enter the admin password" -AsSecureString
+$dnsLabelPrefix = Read-Host -Prompt "Enter the DNS label prefix"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
-  -Name devenvironment `
-  -ResourceGroupName myResourceGroupDev `
-  -TemplateFile $templateFile `
- 
+    -ResourceGroupName $resourceGroupName `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix `
+    -TemplateFile "$HOME/azuredeploy.json"
+Write-Host "Press [ENTER] to continue ..."
